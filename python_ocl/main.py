b11171eeabcc3bcb699ba60bc467312008cb3156
leulshawel <leulshawell@gmail.com>
2026-07-01 20:32:06 +0300

    add & sub pass tet

--- python_ocl/main.py
+++ python_ocl/main.py
@@ -1,8 +1,11 @@
 from pyopencl import _cl as cl
+
 import numpy as np
-import os
-from typing import Tuple
 from math import prod
+
+from typing import Tuple
+from dataclasses import dataclass
+
 
 
 
@@ -36,10 +39,10 @@ class Ops:
 
     matmul = cl.Kernel(_progs, "matmul")
     add = cl.Kernel(_progs, "add")
+    sub = cl.Kernel(_progs, "sub")
 
 
 
-from dataclasses import dataclass
 
 @dataclass
 class Dtype:
@@ -52,7 +55,28 @@ float32 = Dtype("float32", 4)
 int32 = Dtype("int32", 4)
 
 
-class Matrice:
+
+#this is just for code readability
+class Premitive:
+    def __matmul__(self, other):
+        return self.matmul(other)
+    
+    def __add__(self, other):
+        return self.add(other)
+    
+    def __sub__(self, other):
+        return self.sub(other)
+    
+    def __eq__(self, other):
+        return self.np == other.np
+    
+    @property
+    def np(self):
+        n, _ = cl.enqueue_map_buffer(q, self.buff, flags=cl.map_flags.WRITE, offset=0, shape=self.shape, dtype=np.float32)
+        return n
+
+
+class Matrice(Premitive):
     def __init__(self, buff: cl.Buffer, dtype: Dtype, shape: Tuple[int]):
         self.shape = shape
         self.buff = buff
@@ -63,24 +87,17 @@ class Matrice:
     def rand_int(cls, start: int, end: int,  shape: Tuple[int]):
         _buf = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.ALLOC_HOST_PTR, size=prod(shape) * 4)
         buf, _ = cl.enqueue_map_buffer(q, _buf, flags=cl.map_flags.WRITE, offset=0, shape=shape, dtype=np.float32) 
-        q.finish()
         buf[:] = np.random.randint(start, end, shape).astype(np.float32)
         return Matrice(_buf, int32, shape)
     
     @classmethod
     def rand_float(cls, shape: Tuple[int]):
-        buf = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.ALLOC_HOST_PTR, size=prod(shape) * 4)
+        _buf = cl.Buffer(context, cl.mem_flags.READ_WRITE | cl.mem_flags.ALLOC_HOST_PTR, size=prod(shape) * 4)
         buf, _ = cl.enqueue_map_buffer(q, buf, flags=cl.map_flags.WRITE, offset=0, shape=shape, dtype=np.float32) 
-        q.finish()
         buf[:] = np.random.rand(*shape, shape).astype(np.float32)
-        return Matrice(buf, float32, shape)
+        return Matrice(_buf, float32, shape)
 
 
-    def __matmul__(self, other):
-        return self.matmul(other)
-    
-    def __eq__(self, other):
-        return self.np == other.np
 
     def matmul(self, other: "Matrice") -> "Matrice":
         _buff = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.ALLOC_HOST_PTR, size=self.size)
@@ -90,6 +107,7 @@ class Matrice:
         Ops.matmul.set_arg(1, other.buff)
         Ops.matmul.set_arg(2, _buff)
         Ops.matmul.set_arg(3, np.int32(self.shape[1]))
+
         cl.enqueue_nd_range_kernel(q, Ops.matmul, global_work_size=(self.shape[0], other.shape[0]), local_work_size=(1, 1))
         q.finish()
 
@@ -97,32 +115,41 @@ class Matrice:
     
     def add(self, other: "Matrice")->"Matrice":
         buff = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.ALLOC_HOST_PTR, size=self.size)
-        # res, _ = cl.enqueue_map_buffer(q, buff, flags=cl.map_flags.WRITE, offset=0, shape=self.shape, dtype=np.float32)
         Ops.add.set_arg(0, self.buff)
         Ops.add.set_arg(1, other.buff)
+        Ops.add.set_arg(2, buff)
 
         cl.enqueue_nd_range_kernel(q, Ops.add, global_work_size=self.shape, local_work_size=(1, 1))
         q.finish()
 
         return Matrice(buff, self.dtype, self.shape)
     
-    @property
-    def np(self):
-        n, _ = cl.enqueue_map_buffer(q, self.buff, flags=cl.map_flags.WRITE, offset=0, shape=self.shape, dtype=np.float32)
-        return n
+    
+    
+    def sub(self, other) -> "Matrice":
+        buff = cl.Buffer(context, cl.mem_flags.READ_ONLY | cl.mem_flags.ALLOC_HOST_PTR, size=self.size)
+        # res, _ = cl.enqueue_map_buffer(q, buff, flags=cl.map_flags.WRITE, offset=0, shape=self.shape, dtype=np.float32)
+        Ops.sub.set_arg(0, self.buff)
+        Ops.sub.set_arg(1, other.buff)
+        Ops.sub.set_arg(2, buff)
+
+        cl.enqueue_nd_range_kernel(q, Ops.sub, global_work_size=self.shape, local_work_size=(1, 1))
+        q.finish()
+
+        return Matrice(buff, self.dtype, self.shape)
+
 
 
 a = Matrice.rand_int(1, 5, (4, 4))
 b = Matrice.rand_int(1, 5, (4, 4))
 
-
-c = a @ b
+c = a + b
 
 print(a.np)
 print(b.np)
 print(c.np)
 
-print((a.np @ b.np) == c.np)
+print((a.np + b.np) == c.np)
 
 
 
--- rust_ocl/kernels.cl
+++ rust_ocl/kernels.cl
@@ -39,12 +39,14 @@ __kernel void matmul(__global float* a,  __global float* b, __global float* c, i
 
 
 
-//kerne for convolution
-__kernel void conv1d(){
-    
-}
+__kernel void sub(__global float* a, __global float* b, __global float* c) {
+    int gl_id_x  =  get_global_id(0);
+    int gl_id_y  =  get_global_id(1);
+    int width    =  get_global_size(0);
 
-__kernel void sub(__global float* buffer, float scalar) {
-    buffer[get_global_id(0)] -= scalar;
+    int buff_idx =  idx(gl_id_x, width, gl_id_y);
+
+    c[buff_idx] = 0;
+
 }
 
